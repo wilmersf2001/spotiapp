c96dd7b3f736f0a77bfa5abdfee35383d2af6f19
wilmersf2001 <124706662+wilmersf2001@users.noreply.github.com>
2024-04-19 22:30:30 -0600

    Añadir estilos globales de Tailwind CSS y dependencias de PostCSS y Autoprefixer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { NewsComponent } from './pages/news/news.component';
+import { PreferencesComponent } from './pages/preferences/preferences.component';
+import { UserComponent } from './pages/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'playlist', component: PlaylistComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'preferences', component: PreferencesComponent },
+  { path: 'user', component: UserComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/inicio' },
+  { path: '**', pathMatch: 'full', redirectTo: '/inicio' },
+];
